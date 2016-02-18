b6229a6b018b73a2c79759bf1cafe188ed204992
Ben Reynolds <ben.reynolds@ucdconnect.ie>
2016-02-18 15:51:30 +0000

    Initial Commit

---
+++ src/StudentEntry.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class StudentEntry {
+	private String name;
+	private boolean hasPreAssignedProject;
+	private String preAssignedProject;
+	private List<String> projects = new ArrayList<String>();
+	private int statedProjects;
+	private Random random = new Random();
+	
+	public StudentEntry() {
+		
+	}
+	
+	public void setName(String sname) {
+		name = sname;
+	}
+	
+	public String getStudentName() {
+		return name;
+	}
+	
+	public List<String> getOrderedPreferences() {
+		return projects;
+	}
+	
+	public void preassignProject(String pname) {
+		hasPreAssignedProject = true;
+		statedProjects++;
+		preAssignedProject = pname;
+	}
+	
+	public boolean hasPreAssignedProject() {
+		return hasPreAssignedProject;
+	}
+	
+	public int getNumberOfStatedPreferences()	{
+		return statedProjects;
+	}
+	
+	public void addProject(String pname) {
+		projects.add(pname);
+	}
+	
+	public void addOriginalProject(String pname) {
+		projects.add(pname);
+		statedProjects++;
+	}
+	
+	public String getRandomPreference() {
+		String randomProject = projects.get(random.nextInt(projects.size()));
+		return randomProject;
+	}
+	
+	public boolean hasPreference(String preference) {
+		boolean bool = false;
+		for (String key : projects) {
+			   if(key.equalsIgnoreCase(preference) ) {
+				   bool = true;
+			   }
+			}
+		return bool;
+	}
+}
---
+++ src/TestClass.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+
+public class TestClass {	
+		public static void main(String[] args) throws IOException{
+			PreferenceTable table = new PreferenceTable("tabfile.txt");
+			table.setupStudents();
+			System.out.println(table.getAllStudentEntries());
+			
+			// Test Loki Laufeyson
+			System.out.println("\n");
+			StudentEntry student = table.getEntryFor("Loki Laufeyson");
+			System.out.println(student.getStudentName());
+			System.out.println(student.getNumberOfStatedPreferences());
+			System.out.println(student.getOrderedPreferences());
+			System.out.println(student.hasPreAssignedProject());
+			
+			
+			//Test Uriah Heap
+			System.out.println("\n");
+			student = table.getEntryFor("Uriah Heap");
+			System.out.println(student.getStudentName());
+			System.out.println(student.getNumberOfStatedPreferences());
+			System.out.println(student.getOrderedPreferences());
+			System.out.println(student.hasPreAssignedProject());
+			
+			// Test Bridget Jones
+			System.out.println("\n");
+			student = table.getEntryFor("Bridget Jones");
+			System.out.println(student.getStudentName());
+			System.out.println(student.getNumberOfStatedPreferences());
+			System.out.println(student.getOrderedPreferences());
+			System.out.println(student.hasPreAssignedProject());
+			
+			// Random Student Test
+			System.out.println("\nRandom Student:");
+			System.out.println(table.getRandomStudent().getStudentName());
+			
+			// Test Has Preference
+			student = table.getEntryFor("Bridget Jones");
+			System.out.println("\nBridget Jones Has Preference: Recommending Movies Using Curated IMDb Lists?");
+			System.out.println(student.hasPreference("Recommending Movies Using Curated IMDb Lists"));
+			
+			// Test Add Preferences:
+			
+			System.out.println(table.fillPreferencesOfAll(10));
+			
+			
+			
+		}
+	}
+
